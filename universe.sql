c2e8bfe5079910a28c7ac0e52956be0d53b2266b
shridhar h kottappanavar <shridharhk076@gmail.com>
2025-08-09 06:59:59 -0400

    Add final database dump

--- universe.sql
+++ universe.sql
@@ -16,29 +16,6 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
-DROP DATABASE universe;
---
--- Name: universe; Type: DATABASE; Schema: -; Owner: freecodecamp
---
-
-CREATE DATABASE universe WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
-
-
-ALTER DATABASE universe OWNER TO freecodecamp;
-
-\connect universe
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
@@ -280,84 +257,94 @@ ALTER TABLE ONLY public.star ALTER COLUMN star_id SET DEFAULT nextval('public.st
 -- Data for Name: astroids; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.astroids VALUES (4, 'flamingo', NULL);
-INSERT INTO public.astroids VALUES (1, 'flower', 'stem');
-INSERT INTO public.astroids VALUES (2, 'rose', 'flower');
-INSERT INTO public.astroids VALUES (3, 'jasmine', 'plant');
+COPY public.astroids (astroids_id, name, part_of) FROM stdin;
+4	flamingo	\N
+1	flower	stem
+2	rose	flower
+3	jasmine	plant
+\.
 
 
 --
 -- Data for Name: galaxy; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.galaxy VALUES (1, 'milkyway', 'it include solar system', true, true, 1000, 0, 1000);
-INSERT INTO public.galaxy VALUES (2, 'Century', 'dont know it has solar system or not', false, false, 1001, 1000, 1001);
-INSERT INTO public.galaxy VALUES (3, 'double century', 'dont know it has solar system or not', false, false, 1002, 1002, 1002);
-INSERT INTO public.galaxy VALUES (4, 'Triple Century', 'dont know it has solar system or not', false, false, 1003, 1003, 1003);
-INSERT INTO public.galaxy VALUES (5, 'Fifty', 'it include solar system', true, true, 1000, 0, 1000);
-INSERT INTO public.galaxy VALUES (6, 'No Century', 'dont know it has solar system or not', false, false, 1001, 1000, 1001);
-INSERT INTO public.galaxy VALUES (7, 'Fifth century', 'dont know it has solar system or not', false, false, 1002, 1002, 1002);
-INSERT INTO public.galaxy VALUES (8, 'Fourth Century', 'dont know it has solar system or not', false, false, 1003, 1003, 1003);
+COPY public.galaxy (galaxy_id, name, description, has_life, is_sperical, age_in_million_of_years, distance_from_earth, mass) FROM stdin;
+1	milkyway	it include solar system	t	t	1000	0	1000
+2	Century	dont know it has solar system or not	f	f	1001	1000	1001
+3	double century	dont know it has solar system or not	f	f	1002	1002	1002
+4	Triple Century	dont know it has solar system or not	f	f	1003	1003	1003
+5	Fifty	it include solar system	t	t	1000	0	1000
+6	No Century	dont know it has solar system or not	f	f	1001	1000	1001
+7	Fifth century	dont know it has solar system or not	f	f	1002	1002	1002
+8	Fourth Century	dont know it has solar system or not	f	f	1003	1003	1003
+\.
 
 
 --
 -- Data for Name: moon; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.moon VALUES (1, 'frog', 'not part of solar system', true, true, 100, 100, 100, 6);
-INSERT INTO public.moon VALUES (2, 'camal', 'not part of solar system', false, true, 101, 101, 101, 5);
-INSERT INTO public.moon VALUES (3, 'doll', 'not part of solar system', true, true, 1000, 1000, 1000, 4);
-INSERT INTO public.moon VALUES (4, 'boll', 'not part of solar system', true, true, 100, 100, 100, 3);
-INSERT INTO public.moon VALUES (5, 'ant', 'not part of solar system', false, true, 101, 101, 101, 2);
-INSERT INTO public.moon VALUES (6, 'elephent', 'not part of solar system', true, true, 1000, 1000, 1000, 1);
-INSERT INTO public.moon VALUES (7, 'man', 'not part of solar system', true, true, 100, 100, 100, 12);
-INSERT INTO public.moon VALUES (8, 'lion', 'not part of solar system', false, true, 101, 101, 101, 11);
-INSERT INTO public.moon VALUES (9, 'king', 'not part of solar system', true, true, 1000, 1000, 1000, 10);
-INSERT INTO public.moon VALUES (10, 'jockey', 'not part of solar system', true, true, 100, 100, 100, 9);
-INSERT INTO public.moon VALUES (11, 'ili', 'not part of solar system', false, true, 101, 101, 101, 8);
-INSERT INTO public.moon VALUES (12, 'goat', 'not part of solar system', true, true, 1000, 1000, 1000, 7);
-INSERT INTO public.moon VALUES (13, 'owul', 'not part of solar system', true, true, 100, 100, 100, 12);
-INSERT INTO public.moon VALUES (14, 'manga', 'not part of solar system', false, true, 101, 101, 101, 11);
-INSERT INTO public.moon VALUES (15, 'ninga', 'not part of solar system', true, true, 1000, 1000, 1000, 10);
-INSERT INTO public.moon VALUES (16, 'nagin', 'not part of solar system', true, true, 100, 100, 100, 9);
-INSERT INTO public.moon VALUES (17, 'nocknock', 'not part of solar system', false, true, 101, 101, 101, 8);
-INSERT INTO public.moon VALUES (18, 'navilu', 'not part of solar system', true, true, 1000, 1000, 1000, 7);
-INSERT INTO public.moon VALUES (19, 'raja', 'Queen of solar system', true, true, 100, 100, 100, 6);
-INSERT INTO public.moon VALUES (20, 'qick', 'part of solar system', false, true, 101, 101, 101, 5);
-INSERT INTO public.moon VALUES (21, 'parrot', 'part of solar system', true, true, 1000, 1000, 1000, 4);
+COPY public.moon (moon_id, name, description, has_life, is_sperical, age_in_million_of_years, distance_from_earth, mass, planet_id) FROM stdin;
+1	frog	not part of solar system	t	t	100	100	100	6
+2	camal	not part of solar system	f	t	101	101	101	5
+3	doll	not part of solar system	t	t	1000	1000	1000	4
+4	boll	not part of solar system	t	t	100	100	100	3
+5	ant	not part of solar system	f	t	101	101	101	2
+6	elephent	not part of solar system	t	t	1000	1000	1000	1
+7	man	not part of solar system	t	t	100	100	100	12
+8	lion	not part of solar system	f	t	101	101	101	11
+9	king	not part of solar system	t	t	1000	1000	1000	10
+10	jockey	not part of solar system	t	t	100	100	100	9
+11	ili	not part of solar system	f	t	101	101	101	8
+12	goat	not part of solar system	t	t	1000	1000	1000	7
+13	owul	not part of solar system	t	t	100	100	100	12
+14	manga	not part of solar system	f	t	101	101	101	11
+15	ninga	not part of solar system	t	t	1000	1000	1000	10
+16	nagin	not part of solar system	t	t	100	100	100	9
+17	nocknock	not part of solar system	f	t	101	101	101	8
+18	navilu	not part of solar system	t	t	1000	1000	1000	7
+19	raja	Queen of solar system	t	t	100	100	100	6
+20	qick	part of solar system	f	t	101	101	101	5
+21	parrot	part of solar system	t	t	1000	1000	1000	4
+\.
 
 
 --
 -- Data for Name: planet; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.planet VALUES (1, 'jupiter', 'Queen of solar system', true, true, 100, 100, 100, 3);
-INSERT INTO public.planet VALUES (2, 'mars', 'part of solar system', false, true, 101, 101, 101, 2);
-INSERT INTO public.planet VALUES (3, 'earth', 'living planet solar system', true, true, 1000, 1000, 1000, 1);
-INSERT INTO public.planet VALUES (4, 'pluto', 'Queen of solar system', true, true, 100, 100, 100, 6);
-INSERT INTO public.planet VALUES (5, 'neptune', 'part of solar system', false, true, 101, 101, 101, 5);
-INSERT INTO public.planet VALUES (6, 'urenus', 'part of solar system', true, true, 1000, 1000, 1000, 4);
-INSERT INTO public.planet VALUES (7, 'brum', 'not part of solar system', true, true, 100, 100, 100, 6);
-INSERT INTO public.planet VALUES (8, 'crum', 'not part of solar system', false, true, 101, 101, 101, 5);
-INSERT INTO public.planet VALUES (9, 'crim', 'not part of solar system', true, true, 1000, 1000, 1000, 4);
-INSERT INTO public.planet VALUES (10, 'dog', 'not part of solar system', true, true, 100, 100, 100, 3);
-INSERT INTO public.planet VALUES (11, 'cat', 'not part of solar system', false, true, 101, 101, 101, 2);
-INSERT INTO public.planet VALUES (12, 'ban', 'not part of solar system', true, true, 1000, 1000, 1000, 1);
+COPY public.planet (planet_id, name, description, has_life, is_sperical, age_in_million_of_years, distance_from_earth, mass, star_id) FROM stdin;
+1	jupiter	Queen of solar system	t	t	100	100	100	3
+2	mars	part of solar system	f	t	101	101	101	2
+3	earth	living planet solar system	t	t	1000	1000	1000	1
+4	pluto	Queen of solar system	t	t	100	100	100	6
+5	neptune	part of solar system	f	t	101	101	101	5
+6	urenus	part of solar system	t	t	1000	1000	1000	4
+7	brum	not part of solar system	t	t	100	100	100	6
+8	crum	not part of solar system	f	t	101	101	101	5
+9	crim	not part of solar system	t	t	1000	1000	1000	4
+10	dog	not part of solar system	t	t	100	100	100	3
+11	cat	not part of solar system	f	t	101	101	101	2
+12	ban	not part of solar system	t	t	1000	1000	1000	1
+\.
 
 
 --
 -- Data for Name: star; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.star VALUES (1, 'sun', 'king of solar system', true, true, 100, 100, 100, 1);
-INSERT INTO public.star VALUES (2, 'tan', 'near to solar system', false, true, 101, 101, 101, 1);
-INSERT INTO public.star VALUES (3, 'tun', 'away from solar system', true, true, 1000, 1000, 1000, 2);
-INSERT INTO public.star VALUES (4, 'junjun', 'king of solar system', true, true, 100, 100, 100, 3);
-INSERT INTO public.star VALUES (5, 'jun', 'near to solar system', false, true, 101, 101, 101, 3);
-INSERT INTO public.star VALUES (6, 'jan', 'away from solar system', true, true, 1000, 1000, 1000, 2);
-INSERT INTO public.star VALUES (7, 'April', 'king of solar system', true, true, 100, 100, 100, 6);
-INSERT INTO public.star VALUES (8, 'feb', 'near to solar system', false, true, 101, 101, 101, 5);
-INSERT INTO public.star VALUES (9, 'march', 'away from solar system', true, true, 1000, 1000, 1000, 4);
+COPY public.star (star_id, name, description, has_life, is_sperical, age_in_million_of_years, distance_from_earth, mass, galaxy_id) FROM stdin;
+1	sun	king of solar system	t	t	100	100	100	1
+2	tan	near to solar system	f	t	101	101	101	1
+3	tun	away from solar system	t	t	1000	1000	1000	2
+4	junjun	king of solar system	t	t	100	100	100	3
+5	jun	near to solar system	f	t	101	101	101	3
+6	jan	away from solar system	t	t	1000	1000	1000	2
+7	April	king of solar system	t	t	100	100	100	6
+8	feb	near to solar system	f	t	101	101	101	5
+9	march	away from solar system	t	t	1000	1000	1000	4
+\.
 
 
 --
